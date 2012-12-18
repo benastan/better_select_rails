2f9af97a8ac1ff31c63c57368cdee7e522909e69
benastan <bennyjbergstein@gmail.com>
2012-12-18 00:13:03 -0800

    Finishing out loading shit.

--- lib/better_select.rb
+++ lib/better_select.rb
@@ -15,4 +15,24 @@ module BetterSelect
       raise BetterSelect::FrameworkNotFound < StandardError
     end
   end
+
+  private
+
+  def self.asset_pipeline?
+    defined?(::Rails) && ::Rails.version >= '3.1.0'
+  end
+
+  def self.compass?
+    defined?(::Compass)
+  end
+
+  def self.register_compass_extension
+    base = File.join(File.dirname(__FILE__), '..')
+    styles = File.join(base, 'vendor', 'assets', 'stylesheets')
+    ::Compass::Frameworks.register('better_select', :stylesheets_directory => styles)
+  end
+
+  def self.register_rails_engine
+    require 'better_select/engine'
+  end
 end
---
+++ lib/better_select/engine.rb
@@ -0,0 +1,7 @@
+module BetterSelect
+  module Rails
+    class Engine < ::Rails::Engine
+      # La da dee da da da
+    end
+  end
+end
