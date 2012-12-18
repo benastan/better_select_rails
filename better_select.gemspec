7ea309dfb7961082e427e9fd67132d8923b030af
benastan <bennyjbergstein@gmail.com>
2012-12-18 11:13:29 -0800

    Making shit work.

--- better_select.gemspec
+++ better_select.gemspec
@@ -5,12 +5,15 @@ Gem::Specification.new do |gem|
   gem.authors       = ["benastan"]
   gem.email         = ["bennyjbergstein@gmail.com"]
   gem.description   = "A better HTMLSelectElement replacement"
-  gem.summary   = "A better HTMLSelectElement replacement"
+  gem.summary   = "A better HTMLSelectElement replacement, for Rails"
+  gem.homepage = "http://www.benjaminberstein.com"
 
-  gem.files         = `git ls-files`.split($\)
   gem.executables   = gem.files.grep(%r{^bin/}).map{ |f| File.basename(f) }
   gem.test_files    = gem.files.grep(%r{^(test|spec|features)/})
   gem.name          = "better_select"
   gem.require_paths = ["lib", "vendor"]
+  gem.files         = `git ls-files`.split($\) + Dir["vendor/**/*"]
   gem.version       = BetterSelect::VERSION
+
+  gem.add_dependency "railties", "~> 3.1"
 end
--- lib/better_select.rb
+++ lib/better_select.rb
@@ -1,38 +1,5 @@
 require "better_select/version"
+require "better_select/engine"
 
 module BetterSelect
-  class FrameworkNotFound < StandardError; end
-
-  def self.load!
-    if compass? && asset_pipeline?
-      register_compass_extension
-      register_rails_engine
-    elsif compass?
-      register_rails_engine
-    elsif asset_pipeline?
-      register_compass_extension
-    else
-      raise BetterSelect::FrameworkNotFound < StandardError
-    end
-  end
-
-  private
-
-  def self.asset_pipeline?
-    defined?(::Rails) && ::Rails.version >= '3.1.0'
-  end
-
-  def self.compass?
-    defined?(::Compass)
-  end
-
-  def self.register_compass_extension
-    base = File.join(File.dirname(__FILE__), '..')
-    styles = File.join(base, 'vendor', 'assets', 'stylesheets')
-    ::Compass::Frameworks.register('better_select', :stylesheets_directory => styles)
-  end
-
-  def self.register_rails_engine
-    require 'better_select/engine'
-  end
 end
